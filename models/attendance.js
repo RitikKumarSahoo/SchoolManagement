5aa8d88d0e11059f240995d881bceff0fc2f8b07
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-09-25 12:05:45 +0530

    create attendance model

---
+++ models/attendance.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const attendanceSchema = new mongoose.Schema({
+  _school: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+
+  _class: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Class",
+  },
+
+  section: { type: String },
+
+  _teacher: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+
+  presentIds: [
+    {
+      _student: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    },
+  ],
+
+  date: {
+    type: Date,
+  },
+});
+
+module.exports = mongoose.model("Attendance", attendanceSchema);
