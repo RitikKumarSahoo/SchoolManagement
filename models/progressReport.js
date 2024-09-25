05403ff24479f4906476812e201dd3dc07d97626
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-09-25 13:12:48 +0530

    create progressReport model

---
+++ models/progressReport.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const progressReportSchema = new mongoose.Schema(
+  {
+    _school: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "School",
+    },
+
+    _user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    academicYear: {
+      type: String,
+    },
+
+    termType: {
+      type: String,
+      enum: ["midterm", "final"],
+    },
+
+    subjects: [
+      {
+        subject: {
+          type: String,
+        },
+        marks: {
+          type: Number,
+        },
+        examDate: {
+          type: Date,
+        },
+      },
+    ],
+
+    totalMarks: {
+      type: Number,
+    },
+
+    totalOutOf: {
+      type: Number,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("ProgressReport", progressReportSchema);
