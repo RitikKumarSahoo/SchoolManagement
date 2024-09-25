894039a75e0f3fece59919854e23ab51dc32b0a5
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-09-25 13:27:00 +0530

    add _thread field in chatMessage model

--- models/chatMessage.js
+++ models/chatMessage.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 
 const chatMessageSchema = new mongoose.Schema({
+  _thread: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "ChatThread",
+    required: true,
+  },
+
   _from: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
