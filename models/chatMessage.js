6ff747d2a3d1b08345a196baeb3e1833c3abe0e3
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-09-25 13:25:02 +0530

    create chatMessage model

---
+++ models/chatMessage.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const chatMessageSchema = new mongoose.Schema({
+  _from: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+
+  _to: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+
+  text: {
+    type: String,
+  },
+
+  isRead: {
+    type: Boolean,
+    default: false,
+  },
+
+  createdAt: {
+    type: Date,
+  },
+});
+
+module.exports = mongoose.model("ChatMessage", chatMessageSchema);
