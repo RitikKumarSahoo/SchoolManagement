6bb446f4bd2aabe38b2eacdf07ce643dd9104e49
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-09-26 18:18:56 +0530

    create  pendingFees routes

--- routes/rest/index.js
+++ routes/rest/index.js
@@ -1,20 +1,32 @@
-const express = require("express")
-const router = express.Router()
-const expressJwt = require("express-jwt")
-const checkJwt = expressJwt({ secret: process.env.SECRET, algorithms: ['RS256'] }) // the JWT auth check middleware
+const express = require("express");
+const router = express.Router();
+const expressJwt = require("express-jwt");
+const checkJwt = expressJwt({
+  secret: process.env.SECRET,
+  algorithms: ["RS256"],
+}); // the JWT auth check middleware
 
-const users = require("./users")
-const login = require("./auth")
-const signup = require("./auth/signup")
-const forgotpassword = require("./auth/password")
+const users = require("./users");
+const login = require("./auth");
+const signup = require("./auth/signup");
+const forgotpassword = require("./auth/password");
+const school = require("../rest/school");
+const transaction = require("../rest/transaction");
 
-router.post("/login", login.post) // UNAUTHENTICATED
-router.post("/signup", signup.post) // UNAUTHENTICATED
-router.post("/forgotpassword", forgotpassword.startWorkflow) // UNAUTHENTICATED; AJAX
-router.post("/resetpassword", forgotpassword.resetPassword) // UNAUTHENTICATED; AJAX
+router.post("/login", login.post); // UNAUTHENTICATED
+router.post("/signup", signup.post); // UNAUTHENTICATED
+router.post("/forgotpassword", forgotpassword.startWorkflow); // UNAUTHENTICATED; AJAX
+router.post("/resetpassword", forgotpassword.resetPassword); // UNAUTHENTICATED; AJAX
 
-router.all("*", checkJwt) // use this auth middleware for ALL subsequent routes
+//school
+router.post("/createschool", school.Post);
 
-router.get("/user/:id", users.get)
+//transaction
+router.post("/transaction/create", transaction.studentTransaction);
+router.get("/transaction/pendingfee", transaction.pendingPayment);
 
-module.exports = router
+router.all("*", checkJwt); // use this auth middleware for ALL subsequent routes
+
+router.get("/user/:id", users.get);
+
+module.exports = router;
