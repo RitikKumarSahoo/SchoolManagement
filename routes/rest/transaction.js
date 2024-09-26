8322acab39b9ff79cebdd19e4352f997e75968a1
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-09-26 17:59:03 +0530

    create pending fees reminder

--- routes/rest/transaction.js
+++ routes/rest/transaction.js
@@ -1,14 +1,28 @@
 const User = require("../../models/user/index");
 const Transaction = require("../../models/transaction");
 const mail = require("../../lib/mail");
+const moment = require("moment");
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: true,
+  auth: {
+    user: process.env.SMTP_AUTH_USER,
+    pass: process.env.SMTP_AUTH_PASSWORD,
+  },
+});
 
 module.exports = {
   async pendingPayment(req, res) {
     try {
-      const { _school } = req.body;
+      // const { _school } = req.body;
 
-      // all students
-      const students = User.find({ _school, loginType: "student" }).exec();
+      // Fetch all students in the school
+      const students = await User.find({
+        loginType: "student",
+      }).exec();
 
       if (students.length === 0) {
         return res
@@ -16,33 +30,35 @@ module.exports = {
           .json({ error: true, message: "No students found" });
       }
 
-      //pending fees
       const studentsWithPendingPayments = [];
+
       for (const student of students) {
+        let totalAmount = 0;
         const pendingTransactions = await Transaction.find({
           _user: student._id,
           status: "pending",
-        }).exec();
+        });
 
-        //extract months of a student
         if (pendingTransactions.length > 0) {
-          const pendingMonths = pendingTransactions.map(
-            (transaction) => transaction.month
-          );
+          const pendingMonths = pendingTransactions.map((transaction) => {
+            totalAmount += transaction.amount;
+            return moment(transaction.date).format("MMMM");
+          });
 
           studentsWithPendingPayments.push({
             studentId: student._id,
             pendingMonths,
-            studentemail: student.email,
+            studentEmail: student.email,
+            amountDue: totalAmount,
           });
 
-          // send email to each student for their pending fees
-          mail("pending-fees", {
+          await mail("pending-fees", {
             to: student.email,
-            subject,
+            subject: "Pending Fee Reminder",
             locals: {
-              studentName: student.firstname,
+              studentName: student.firstName,
               pendingMonths,
+              totalAmount,
             },
           });
         }
@@ -54,6 +70,25 @@ module.exports = {
       });
     } catch (error) {
       return res.status(500).json({ error: true, message: error.message });
+    }
+  },
+
+  // student transaction
+  async studentTransaction(req, res) {
+    try {
+      const newTransaction = await Transaction.create(req.body);
+
+      res.status(201).json({
+        error: false,
+        transaction: newTransaction,
+      });
+    } catch (error) {
+      console.log(error.message);
+
+      res.status(400).json({
+        message: "Error creating transaction",
+        error: error.message,
+      });
     }
   },
 };
