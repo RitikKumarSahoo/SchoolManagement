926d5d4cc22c7c2164ba4446ffcbe571ba80eb74
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-09-27 12:21:46 +0530

    add createteacher route

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,12 +1,16 @@
-const express = require("express")
-const router = express.Router()
-const expressJwt = require("express-jwt")
-const checkJwt = expressJwt({ secret: process.env.SECRET, algorithms: ['RS256'] }) // the JWT auth check middleware
+const express = require("express");
+const router = express.Router();
+const expressJwt = require("express-jwt");
+const checkJwt = expressJwt({
+  secret: process.env.SECRET,
+  algorithms: ["RS256"],
+}); // the JWT auth check middleware
 
-const users = require("./users")
+const teacher = require("./teacher");
 
-router.all("*", checkJwt) // use this auth middleware for ALL subsequent routes
+router.all("*", checkJwt); // use this auth middleware for ALL subsequent routes
 
-router.get("/user/:id", users.get)
+router.get("/user/:id", users.get);
+router.post("/teacher/create", teacher.createTeacher);
 
-module.exports = router
+module.exports = router;
