2a7670715b7930fb2c1fc83d1f8292f1a6e27be3
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-09-28 18:29:29 +0530

    create find class by academicYear or name

--- routes/admin/class.js
+++ routes/admin/class.js
@@ -3,6 +3,7 @@ const School = require("../../models/school");
 const Schedule = require("../../models/schedule");
 const randomstring = require("randomstring");
 const mail = require("../../lib/mail");
+const Class = require("../../models/class");
 
 module.exports = {
   // create class
@@ -23,17 +24,25 @@ module.exports = {
           .json({ error: true, reason: "You are not admin" });
       }
 
-      // check for valid year
-      const isValidAcademicYear = moment(
-        academicYear,
-        "YYYY-YYYY",
-        true
-      ).isValid();
-      if (!isValidAcademicYear) {
-        return res
-          .status(400)
-          .json({ message: "Invalid academic year format. Use YYYY-YYYY." });
+      // Check if class already exists
+      const existingClass = await Class.findOne({ name, section });
+      if (existingClass) {
+        return res.status(409).json({
+          message: "Class with this name and section already exists.",
+        });
       }
+
+      // // check for valid year
+      // const isValidAcademicYear = moment(
+      //   academicYear,
+      //   "YYYY-YYYY",
+      //   true
+      // ).isValid();
+      // if (!isValidAcademicYear) {
+      //   return res
+      //     .status(400)
+      //     .json({ message: "Invalid academic year format. Use YYYY-YYYY." });
+      // }
 
       const classReponse = await Class.create({
         name,
@@ -49,6 +58,50 @@ module.exports = {
       });
     } catch (error) {
       return res.status(500).json({ error: true, error });
+    }
+  },
+
+  // search classes by academicyear or name
+  async find(req, res) {
+    try {
+      const { name, section, academicYear } = req.query;
+
+      const user = await User.findOne({
+        _id: req.user.id,
+        isAdmin: true,
+      });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: true, message: "You are not an admin" });
+      }
+
+      const query = { _school: req.user._school };
+      if (academicYear) {
+        query.academicYear = academicYear;
+      }
+      if (name && section) {
+        query.name = name;
+      }
+
+      const classes = await Class.find(query);
+      if (classes.length === 0) {
+        return res.status(404).json({
+          error: true,
+          message: "No classes found for the given criteria.",
+        });
+      }
+
+      const classCounts = await Class.countDocuments(query).exec();
+
+      return res.status(200).json({
+        error: false,
+        data: classes,
+        classCounts,
+      });
+    } catch (error) {
+      return res.status(400).json({ error: true, reason: error.message });
     }
   },
 };
