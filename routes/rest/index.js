f104aa2971cfb8df84b7cd70b8f0642c2bedbdc8
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-10-21 18:49:24 +0530

    changes in admin route

--- routes/rest/index.js
+++ routes/rest/index.js
@@ -23,7 +23,7 @@ router.all("*", checkJwt); // use this auth middleware for ALL subsequent routes
 
 router.get("/admin/getAll", signup.getAllAdmin);
 router.get("/admin/get/:id", signup.get);
-router.post("/admin/signup", signup.signupByAdmin);
+router.post("/admin/signup", signup.createAdmin);
 router.put("/admin/update/:id", login.updateAdmin);
 router.delete("/admin/delete/:id", signup.deleteAdmin);
 
