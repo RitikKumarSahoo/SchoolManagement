ceec7b713b3d719af44c90e8309be60c36ececc4
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-11-01 19:03:45 +0530

    changes in leave models

---
+++ models/leave.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const leaveSchema = new mongoose.Schema({
+  _school: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "School",
+  },
+
+  _teacher: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+
+  leaveType: {
+    type: String,
+    enum: ["PL", "SL", "CL"],
+  },
+
+  startDate: { type: Date },
+
+  endDate: { type: Date },
+
+  reason: { type: String },
+
+  status: {
+    type: String,
+    enum: ["pending", "approved", "rejected", "cancelled"],
+    default: "pending",
+  },
+
+  appliedDate: { type: Date },
+
+  approvedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+
+  isHalfDay: { type: Boolean },
+});
+
+module.exports = mongoose.model("Leave", leaveSchema);
