66b3c7976d8fb5bed771503d17290df1080b2dc0
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-11-01 19:09:01 +0530

    apidocs

--- public/docs/api_project.js
+++ public/docs/api_project.js
@@ -1,14 +1,14 @@
 define({
-  name: "express-skeleton-3",
-  version: "1.0.0",
-  description: "A Skeleton Application",
-  sampleUrl: false,
-  defaultVersion: "0.0.0",
-  apidoc: "0.3.0",
-  generator: {
-    name: "apidoc",
-    time: "2024-10-30T06:14:37.107Z",
-    url: "https://apidocjs.com",
-    version: "0.29.0",
-  },
+  "name": "express-skeleton-3",
+  "version": "1.0.0",
+  "description": "A Skeleton Application",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2024-11-01T13:38:49.394Z",
+    "url": "https://apidocjs.com",
+    "version": "0.29.0"
+  }
 });
