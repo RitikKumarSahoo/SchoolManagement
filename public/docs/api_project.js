a8cc62144886e663559354644bd30d5d95dbd447
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-11-04 23:25:56 +0530

    changes school apidocs

--- public/docs/api_project.js
+++ public/docs/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2024-11-04T07:15:23.203Z",
+    "time": "2024-11-04T13:11:38.177Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
