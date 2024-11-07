522095fbfbf6b8ffeae393b930967450a9acd663
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-11-07 12:04:57 +0530

    add timeStamp

--- models/attendance.js
+++ models/attendance.js
@@ -1,33 +1,36 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
-const attendanceSchema = new mongoose.Schema({
-  _school: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-
-  _class: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Class",
-  },
-  _teacher: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-
-  presentIds: [
-    {
+const attendanceSchema = new mongoose.Schema(
+  {
+    _school: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
-  ],
-  date: {
-    type: Date,
+
+    _class: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Class",
+    },
+    _teacher: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    presentIds: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    date: {
+      type: Date,
+    },
+    comment: {
+      type: String,
+    },
   },
-  comment: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Attendance", attendanceSchema);
--- models/class.js
+++ models/class.js
@@ -1,33 +1,36 @@
 const mongoose = require("mongoose");
 
-const ClassSchema = new mongoose.Schema({
-  _school: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "School",
-  },
-  name: {
-    type: String,
-  },
-  section: {
-    type: String,
-    enum: ["A", "B", "C", "D", "E", "F"],
-  },
-  academicYear: {
-    type: String,
-  },
+const ClassSchema = new mongoose.Schema(
+  {
+    _school: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "School",
+    },
+    name: {
+      type: String,
+    },
+    section: {
+      type: String,
+      enum: ["A", "B", "C", "D", "E", "F"],
+    },
+    academicYear: {
+      type: String,
+    },
 
-  _schedule: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Schedule",
+    _schedule: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Schedule",
+    },
+    totalClassTill: {
+      type: Number,
+      default: 0,
+    },
+    tillNowAttend: {
+      type: Number,
+      default: 0,
+    },
   },
-  totalClassTill: {
-    type: Number,
-    default: 0,
-  },
-  tillNowAttend: {
-    type: Number,
-    default: 0,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Class", ClassSchema);
