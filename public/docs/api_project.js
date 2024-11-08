f5432d46890afc5c32189a2b9ecabd9a4a1a77ae
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-11-08 15:21:56 +0530

    apidocs

--- public/docs/api_project.js
+++ public/docs/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2024-11-07T06:57:05.754Z",
+    "time": "2024-11-08T09:51:25.696Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
