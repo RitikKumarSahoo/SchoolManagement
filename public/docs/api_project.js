546934b5e52178dd9c35f8bbd1007533525aed34
LogicSquare-1996 <mrinal@logic-square.com>
2024-11-11 17:42:08 +0530

    API oc generated

--- public/docs/api_project.js
+++ public/docs/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2024-11-11T05:54:25.795Z",
+    "time": "2024-11-11T12:11:39.080Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
