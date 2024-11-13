8b4e08549d7eaa804e0f83cb2fd8cc10b777dac1
LogicSquare-1996 <mrinal@logic-square.com>
2024-11-13 17:27:44 +0530

    API Doc modified

--- public/docs/api_project.js
+++ public/docs/api_project.js
@@ -7,7 +7,11 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
+<<<<<<< HEAD
     "time": "2024-11-13T10:01:42.597Z",
+=======
+    "time": "2024-11-13T11:57:27.504Z",
+>>>>>>> 1701198 (API Doc modified)
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
