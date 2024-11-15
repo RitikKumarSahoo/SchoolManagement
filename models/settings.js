ae11788076853a216bd79fea0c4b57f914dec12e
RitikKumarSahoo <ritik@logic-square.com>
2024-11-15 18:45:19 +0530

    Revert "implement settings"
    This reverts commit 2224d5fa1889373e82fc7aff7e09b41dd45438b3.

--- models/settings.js
+++ models/settings.js
@@ -4,7 +4,7 @@ const settingSchema = new mongoose.Schema({
   availableClasses: [
     {
       grade: { type: String },
-      sections: [String],
+      section: [String],
       monthlyFee: { type: Number }, // student Fee
     },
   ],
@@ -16,23 +16,20 @@ const settingSchema = new mongoose.Schema({
 
   academicYear: { type: String },
 
-  busFee: [
-    {
-      range: { type: String }, // e.g., "1-5", "6-10"
-      fee: { type: Number },  
-    }
-  ],
-
+  busFee: {
+    type: Map,
+    of: Number,
+  },
   salaryRange: [
     {
-      experienceRange: { type: String }, //"1-4", "5-7", "8-10"
+      classRange: { type: String }, //"1-4", "5-7", "8-10"
       salary: { type: Number },
     },
   ],
 
   holidays: [
     {
-      occasion: { type: String }, // Name of the holiday, e.g., "Diwali"
+      name: { type: String }, // Name of the holiday, e.g., "Diwali"
       date: { type: Date }, // Specific date of the holiday
     },
   ],
@@ -85,8 +82,6 @@ const settingSchema = new mongoose.Schema({
       }, { _id: false })
     },
   },
-},
-{timestamps:true}
-);
+});
 
 module.exports = mongoose.model("Setting", settingSchema);
