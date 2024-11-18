00faed162822bd8b2d6484d03d8ba2db3a3705f2
RitikKumarSahoo <ritik@logic-square.com>
2024-11-18 12:04:20 +0530

    apidocs

--- public/docs/api_project.js
+++ public/docs/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2024-11-18T06:27:37.383Z",
+    "time": "2024-11-18T06:33:52.821Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
